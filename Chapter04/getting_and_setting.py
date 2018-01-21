62e148f3abd8b15d26489983756c877eab2e2370
Thomas Reaney <thomas@reaney.net>
2018-01-21 17:09:26 +0000

    Chapter 03: Loading, Displaying, and Saving

--- Chapter04/getting_and_setting.py
+++ Chapter04/getting_and_setting.py
@@ -1,16 +1,5 @@
-# USAGE
-# python getting_and_setting.py --image ../images/trex.png
-
-# Import the necessary packages
-from __future__ import print_function
-import argparse
 import cv2
 
-# Construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required = True,
-	help = "Path to the image")
-args = vars(ap.parse_args())
 
 # Load the image and show it
 image = cv2.imread(args["image"])
